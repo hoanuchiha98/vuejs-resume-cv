1d164e08440262f851a80ba68e8859773b428a62
hoannk <hoanuchiha98@gmail.com>
2021-03-25 20:45:05 +0700

    first cv

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Layout id="app">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </Layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Layout from "@/components/Layout"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
+@import "demo/demo.css";
+@import "assets/css/styles.css";
+@import "assets/css/responsive.css";
+/*@import "assets/css/colors/purple.css";*/
+@import "assets/css/colors/orange.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n';
+import vnMessage from './locales/vn.json'
+import enMessage from './locales/en.json'
 
+Vue.use(VueI18n)
+
+const messages = {
+  vn: vnMessage,
+  en: enMessage
+}
+const i18n = new VueI18n({
+  locale: 'vn',
+  messages,
+  fallbackLocale: 'vn'
+})
 Vue.config.productionTip = false
-
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')
